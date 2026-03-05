24a21d593eaea4205581e8d5e99d32877824d339
ITproover <141505852+ITproover@users.noreply.github.com>
2026-03-05 15:52:07 +0200

    navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
+import { t } from "../i18n";
 
 const Navigation = ({ language = "ru", onLanguageChange }) => {
 
